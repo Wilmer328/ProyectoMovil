10c22d18f2c8da06e90c9685ca8405fa5e876f87
Wilmer415 <wilmer415sanchez@gmail.com>
2025-01-30 16:08:03 -0600

    Actualizacion Trabjo en clase. 29 enero

---
+++ Proyecto1/app/(masters)/_layout.tsx
@@ -0,0 +1,25 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native"
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native"
+import { StatusBar } from "expo-status-bar";
+
+export default function MastersLayout(){
+
+    const colorScheme = useColorScheme();
+
+    return(
+
+<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme }>
+    <Stack
+    screenOptions={{
+        headerShown: false,
+    }}
+    >
+        <Stack.Screen name="registration" options={{title: "Registro" }}/>
+    </Stack>
+    <StatusBar style="auto"/>
+
+</ThemeProvider>
+
+    );
+}
